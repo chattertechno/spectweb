4eaed5c7dc3a16cf7d93b088211d34ea31641a2f
Chatter Tech <reneekrom28@gmail.com>
2020-09-11 16:26:35 +0300

    made some changes

--- config.js
+++ config.js
@@ -1,34 +1,34 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Spectral', // <title>
-  manifestName: 'Spectral',
+  siteTitle: 'RENEE KROM WEBSITE', // <title>
+  manifestName: 'Renee krom ',
   manifestShortName: 'Landing', // max 12 characters
   manifestStartUrl: '/',
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-spectral/`, // This path is subpath of your hosting https://domain/portfolio
-  heading: 'Anubhav Srivastava',
-  subHeading: 'Full time Web Developer. Part time Open source contributor  ',
+  pathPrefix: `/reneekrom/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'RENEE KROM',
+  subHeading: 'Full time Web And Mobile Developer. Devoted to help people in all Matter of Life  ',
   // social
   socialLinks: [
     {
       style: 'brands',
       icon: 'fa-github',
       name: 'Github',
-      url: 'https://github.com/anubhavsrivastava',
+      url: 'https://github.com/chattertechno',
     },
     {
       style: 'brands',
       icon: 'fa-twitter',
       name: 'Twitter',
-      url: 'https://twitter.com/onlyanubhav',
+      url: 'https://twitter.com/ChatterRenrie',
     },
     {
       style: 'solid',
       icon: 'fa-envelope',
       name: 'Email',
-      url: 'mailto:test@example.com',
+      url: 'mailto:reneekrom11@gmail.com',
     },
   ],
 };
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,10 +16,7 @@ export default function Footer() {
         })}
       </ul>
       <ul className="copyright">
-        <li>&copy; Spectral</li>
-        <li>
-          Design: <a href="http://html5up.net">HTML5 UP</a>
-        </li>
+        <li>&copy;2020 ALL RIGHTS RESERVED | RENEE KROM</li>
       </ul>
     </footer>
   );
